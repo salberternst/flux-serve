4405c4de09e78a3806d39b76b65137e0aaa6fcdc
Sebastian Alberternst <alberternst@gmail.com>
2025-05-16 12:43:13 +0200

    Revert init changes

--- app/init.py
+++ app/init.py
@@ -1,8 +1,11 @@
 from main import Settings, TextToImagePipeline
-from huggingface_hub import snapshot_download
 
 
 if __name__ == "__main__":
     settings = Settings()
 
-    snapshot_download(repo_id=settings.model_name)
+    TextToImagePipeline(
+        model_name=settings.model_name,
+        device=settings.device,
+        dtype=settings.dtype
+    )
