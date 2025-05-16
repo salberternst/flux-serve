70209acfbe49b2b6c334443d8b00711d3429d15b
Sebastian Alberternst <alberternst@gmail.com>
2025-05-16 12:41:55 +0200

    Update chart

--- app/init.py
+++ app/init.py
@@ -1,11 +1,8 @@
 from main import Settings, TextToImagePipeline
+from huggingface_hub import snapshot_download
 
 
 if __name__ == "__main__":
     settings = Settings()
 
-    TextToImagePipeline(
-        model_name=settings.model_name,
-        device=settings.device,
-        dtype=settings.dtype
-    )
+    snapshot_download(repo_id=settings.model_name)
